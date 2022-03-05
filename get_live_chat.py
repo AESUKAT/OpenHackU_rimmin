4074eeb6e05b714831a7eae66fa4cd182e1ebd96
yukichi <yuki121102@icloud.com>
2022-03-05 19:43:58 +0900

    updated get_live_chat.py

--- get_live_chat.py
+++ get_live_chat.py
@@ -1,28 +1,63 @@
 import pytchat
 import time
 import json
-# PytchatCoreオブジェクトの取得
+import os
+
+chat_max_cnt = 32760
+
+def makeNewFile(file_path):
+    with open(file_path, 'w') as f:
+        pass
 
 def getLiveChat(video_id):
     livechat = pytchat.create(video_id=video_id)
 
-    file_path = f'comment_data/{video_id}.json'
+    # 一つ一つのコメントに対してユニークな値をあたえる必要があるから
+    # コメントを約2^15個ごとにファイルを分けている。
+    # chat_cntはファイル内のコメントのindex。
+    chat_cnt = 0
+    file_cnt = 0
 
-    with open(file_path, 'w') as f:
-        pass
+    dir_path = f'chat_data/{video_id}'
+
+    if os.path.exists(dir_path):
+        print('dir already exists')
+    else:
+        print('made a new dir')
+        os.mkdir(dir_path)
+
+    file_path = f'chat_data/{video_id}/chat_file_{file_cnt:0>4}.json'
+
+    makeNewFile(file_path)
 
     while livechat.is_alive():
         # チャットデータの取得
         chatdata = livechat.get()
 
-        comment_data = []
+        temp_data = dict()
+
+        if chat_cnt == 0:
+            chat_data = dict()
+        else:
+            with open(file_path, 'r') as f:
+                chat_data = json.load(f)
 
         for c in chatdata.items:
             print(f"{c.datetime} {c.author.name} {c.message} {c.amountString}")
-            comment_data.append({'datetime': c.datetime, 'author.name': c.author.name, 'message': c.message, 'amountString': c.amountString})
+            temp_data[f'chat{chat_cnt}'] = {'datetime': c.datetime, 'author.name': c.author.name, 'message': c.message, 'amountString': c.amountString}
+            chat_cnt += 1
 
-        with open(file_path, 'a') as f:
-            json.dump(comment_data, f, indent=4)
+        chat_data.update(temp_data)
+
+        with open(file_path, 'w') as f:
+            json.dump(chat_data, f, indent=4)
+
+        if chat_cnt >= chat_max_cnt:
+            chat_cnt = 0
+            file_cnt += 1
+            file_path = f'chat_data/{video_id}/chat_file_{file_cnt:0>4}.json'
+            makeNewFile(file_path)
+
         time.sleep(5)
 
 if __name__ == '__main__':
