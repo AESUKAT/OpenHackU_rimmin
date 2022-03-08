71791c384f0f9857b57c496e64248b69a33c7be8
ne0nblue <c0119290d9@edu.edu.ac.jp>
2022-03-08 23:55:30 +0900

    update superchat.py

--- superChat.py
+++ superChat.py
@@ -1,6 +1,7 @@
 import pytchat
 import time
 import json
+import numpy
 
 def loadJson(video_id,file_cnt):
     file_path = f'chat_data/{video_id}/chat_file_{file_cnt:0>4}.json'
@@ -9,7 +10,7 @@ def loadJson(video_id,file_cnt):
         chat_data = json.load(f)
     return chat_data
 
-def countSectionSuperChat(chat_data,interval_time = 10000):
+def countSectionSuperChat(chat_data,interval_time = 10):
     '''
     sectioned_superchat_cnt : 一定区間ごとのスーパーチャットの個数のリスト
     base_time : 参照中の区間の開始時間
@@ -19,16 +20,25 @@ def countSectionSuperChat(chat_data,interval_time = 10000):
     sectioned_superchat_cnt.append(0)
 
     for key in chat_data.keys():
-        if base_time + interval_time < chat_data[key]["timestamp"]:
-            base_time += interval_time
-            sectioned_superchat_cnt.append(0)
-
+        elapsed_time = chat_data[key]["elapsedTime"]
+        elapsed_time = elapsed_time.split(":")
+        now_time = 0
+        for i in range(len(elapsed_time)):
+            now_time += (60**i)*int(elapsed_time[-(1+i)])
+        
         if chat_data[key]["amountString"] != "":
-            sectioned_superchat_cnt[-1] += 1
+            if len(sectioned_superchat_cnt) <= now_time:
+                tmp = [0 for i in range(now_time - len(sectioned_superchat_cnt) + 1)]
+                sectioned_superchat_cnt.extend(tmp)
+                sectioned_superchat_cnt[now_time] += 1
+            print(elapsed_time, chat_data[key]["amountString"])
+    print(sectioned_superchat_cnt)
+    
+    return numpy.array(sectioned_superchat_cnt)
 
-    return sectioned_superchat_cnt
 if __name__ == "__main__":
-    VIDEO_ID = "JPFFHu1X3gY"
+    VIDEO_ID = "EY62MO3bOPE"
     FILE_CNT = 0
     chat_data = loadJson(VIDEO_ID, FILE_CNT)
-    print(countSectionSuperChat(chat_data))
+    
+    countSectionSuperChat(chat_data)
