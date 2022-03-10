ec6a718d81369302caede0c90640cae9895b971b
KoshinQ <c01193429a@edu.teu.ac.jp>
2022-03-11 02:35:40 +0900

    自動化完了

--- pytailor.py
+++ pytailor.py
@@ -12,11 +12,10 @@ import kukan
 import autoPleyer
 import superChat
 
-video_id = 'JPFFHu1X3gY'
+video_id = input()
 #get_data.get_live_chat(video_id)
 chat_data = cmn_func.import_json_info(video_id)
 funny_point_list = instantaneous.get_inst_higher(chat_data)
-print(funny_point_list)
 
 path_string = "./chat_data/" + video_id + "/chat_file_0000.json"
 section_time = 30 #一区間は何秒か
@@ -27,16 +26,11 @@ ranking_list = kukan.calculation_of_sections_function(jsn, total_time, section_t
 
 FILE_CNT = 0
 super_chat_data = superChat.load_json(video_id, FILE_CNT)
-super_chat_list = [superChat.count_section_super_chat(super_chat_data)]
-print(super_chat_list)
+super_chat_list = superChat.count_section_super_chat(super_chat_data)
 
 concatenate_list = [funny_point_list, ranking_list, super_chat_list]
-print("test", concatenate_list)
-number_line = []
-number_line = cmn_func.time_to_number_line(concatenate_list)
+funny_clip = []
+funny_clip = cmn_func.time_to_funny_clip(concatenate_list)
 
-np.set_printoptions(threshold=60000)
-print(number_line)
-
-#target_url = "https://www.youtube.com/watch?v=" + "JPFFHu1X3gY"
-#autoPleyer.call_selenium(target_url, ranking_list)
+target_url = "https://www.youtube.com/watch?v=" + "JPFFHu1X3gY"
+autoPleyer.call_selenium(target_url, funny_clip)
