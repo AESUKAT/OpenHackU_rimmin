6b662aa9bf0cadfb512192e5116ccd63ef2192d3
yukichi <yuki121102@icloud.com>
2022-03-11 01:44:10 +0900

    update time_to_funny_clip func

--- cmn_func.py
+++ cmn_func.py
@@ -1,8 +1,19 @@
 import json
+from math import comb
 import numpy as np
+import copy
+
+from sympy import is_nthpow_residue
+
 
 START_TIME = 0
 LAST_CHAT_TIME = 0
+
+BEFORE_TIME = 15
+AFTER_TIME = 45
+
+FUNNY_SCORE_BORDER = 2
+
 
 def import_json_info(video_id):
     global LAST_CHAT_TIME
@@ -17,6 +28,7 @@ def import_json_info(video_id):
     
     return chat_data
 
+
 def time_to_seconds(input_time):
     time = list(map(int, input_time.split(':')))
     hour = 0
@@ -27,13 +39,20 @@ def time_to_seconds(input_time):
 
     return hour*60*60 + minute*60 + second
 
+
+def time_to_funny_clip(time_info_list):
+    number_line = time_to_number_line(time_info_list)
+    comb_funny_time = search_time(time_info_list)
+    funny_clip = number_line_to_funny_clip(number_line, comb_funny_time)
+
+    return funny_clip
+
 def time_to_number_line(time_info_list):
     number_line = np.array([0] * LAST_CHAT_TIME)
 
     for time_info in time_info_list:
         temp_number_line = np.array([0] * LAST_CHAT_TIME)
         for time in time_info:
-            print(time)
             if time[0] < START_TIME:
                 time[0] = START_TIME
             if time[1] > LAST_CHAT_TIME:
@@ -43,12 +62,54 @@ def time_to_number_line(time_info_list):
     
     return number_line
 
+
+def search_time(time_info_list):
+    comb_time_info = []
+    for time_info in time_info_list:
+        for time in time_info:
+            comb_time_info.append(time)
+    
+    is_next = True
+    comb_funny_time = []
+    for time in comb_time_info:
+        if is_next:
+            start_time = time[0]
+            finish_time = time[1]
+            is_next = False
+        else:
+            if time[0] < finish_time:
+                finish_time = time[1]
+            else:
+                comb_funny_time.append([start_time, finish_time])
+                is_next = True
+    else:
+        start_time = time[0]
+        finish_time = time[1]
+        comb_funny_time.append([start_time, finish_time])
+
+    return comb_funny_time
+
+
+def number_line_to_funny_clip(number_line, comb_funny_time):
+    funny_clip = []
+    for time in comb_funny_time:
+        if max(number_line[time[0]:time[1]+1]) >= FUNNY_SCORE_BORDER:
+            funny_clip.append(time)
+
+    return funny_clip
+
+
 def main():
+    import instantaneous
+
     # video_id = input('plz input video_id:')
     video_id = 'SeWzXV0fYOM'
 
     chat_data = import_json_info(video_id)
-    print(list(chat_data.keys())[:10])
+
+    funny_point_list = [instantaneous.get_inst_higher(chat_data)]
+    funny_clip = time_to_funny_clip(funny_point_list)
+    print(funny_clip)
 
 
 if __name__ == '__main__':
