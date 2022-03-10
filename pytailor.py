8689d3a6c2f53cffe6d95d84d3c10f0d4b42bf8a
KoshinQ <c01193429a@edu.teu.ac.jp>
2022-03-10 23:45:53 +0900

    superchat追加

--- pytailor.py
+++ pytailor.py
@@ -10,6 +10,7 @@ import instantaneous
 import numpy as np
 import kukan
 import autoPleyer
+import superChat
 
 video_id = 'JPFFHu1X3gY'
 get_data.get_live_chat(video_id)
@@ -25,11 +26,16 @@ ranking_list = []
 ranking_list = kukan.calculation_of_sections_function(jsn, total_time, section_time)
 np.set_printoptions(threshold=60000)
 
-continue_list = [funny_point_list, ranking_list]
+FILE_CNT = 0
+super_chat_data = superChat.load_json(video_id, FILE_CNT)
+super_chat_list = superChat.count_section_super_chat(super_chat_data)
+
+continue_list = [funny_point_list, ranking_list, super_chat_data]
 print(continue_list)
 number_line = []
 number_line = cmn_func.time_to_number_line(continue_list)
 print(number_line)
+
 
 #target_url = "https://www.youtube.com/watch?v=" + "JPFFHu1X3gY"
 #autoPleyer.call_selenium(target_url, ranking_list)
