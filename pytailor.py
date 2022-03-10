91f1402d0b54c6f7ba60295c5afd0d8177408195
KoshinQ <c01193429a@edu.teu.ac.jp>
2022-03-10 16:13:23 +0900

    number_lineの適用

--- pytailor.py
+++ pytailor.py
@@ -14,20 +14,22 @@ import autoPleyer
 video_id = 'JPFFHu1X3gY'
 get_data.get_live_chat(video_id)
 chat_data = cmn_func.import_json_info(video_id)
-"""
-chat_data = cmn_func.import_json_info(video_id)
-funny_point_list = [instantaneous.get_inst_higher(chat_data)]
-number_line = []
-number_line = cmn_func.time_to_number_line(funny_point_list)
-np.set_printoptions(threshold=60000)
-print(number_line)
-"""
+funny_point_list = instantaneous.get_inst_higher(chat_data)
+print(funny_point_list)
+
 path_string = "./chat_data/" + video_id + "/chat_file_0000.json"
 section_time = 30 #一区間は何秒か
 jsn = kukan.load_json_function(path_string)
 total_time = kukan.total_time_calculation_function(jsn)
 ranking_list = []
 ranking_list = kukan.calculation_of_sections_function(jsn, total_time, section_time)
+np.set_printoptions(threshold=60000)
 
-target_url = "https://www.youtube.com/watch?v=" + "JPFFHu1X3gY"
-autoPleyer.call_selenium(target_url, ranking_list)
+continue_list = [funny_point_list, ranking_list]
+print(continue_list)
+number_line = []
+number_line = cmn_func.time_to_number_line(continue_list)
+print(number_line)
+
+#target_url = "https://www.youtube.com/watch?v=" + "JPFFHu1X3gY"
+#autoPleyer.call_selenium(target_url, ranking_list)
