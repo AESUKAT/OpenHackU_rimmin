ab3744d76c8fa4c2893cb6f3065ffefd71dca6c0
yukichi <yuki121102@icloud.com>
2022-03-11 17:29:24 +0900

    update instantaneous.py

--- cmn_func.py
+++ cmn_func.py
@@ -1,10 +1,5 @@
 import json
-from math import comb
-from tracemalloc import start
 import numpy as np
-import copy
-
-from sympy import is_nthpow_residue
 
 
 START_TIME = 0
@@ -13,7 +8,7 @@ LAST_CHAT_TIME = 0
 BEFORE_TIME = 5
 AFTER_TIME = 5
 
-FUNNY_SCORE_BORDER = 2
+FUNNY_SCORE_BORDER = 1
 
 
 def import_json_info(video_id):
@@ -98,7 +93,8 @@ def number_line_to_funny_clip(number_line, comb_funny_time):
     for time in comb_funny_time:
         max_score = max(number_line[time[0]:time[1]+1])
         if max_score >= FUNNY_SCORE_BORDER:
-            max_index = number_line.index(max_score)
+            max_index = np.argmax(number_line[time[0]:time[1]+1] >= max_score)
+            max_index += time[0]
             start_time = max_index - BEFORE_TIME
             finish_time = max_index + AFTER_TIME
             if start_time < START_TIME:
--- instantaneous.py
+++ instantaneous.py
@@ -4,13 +4,30 @@ TIME_RANGE = 2
 AVE_TIME_RANGE = 5
 CHAT_INST_SPEED_MAG = 2
 
-BEFORE_TIME = 15
-AFTER_TIME = 45
+CLIP_TIME = 20
+TIME_RATIO = 1/4
+LAG_TIME = 5
+
+BEFORE_TIME = 0
+AFTER_TIME = 0
+
 
 def get_funny_range(funny_point):
     return [funny_point-BEFORE_TIME, funny_point+AFTER_TIME]
 
+
+def set_time():
+    global BEFORE_TIME, AFTER_TIME
+    if CLIP_TIME * TIME_RATIO >= LAG_TIME:
+        BEFORE_TIME = int(CLIP_TIME * TIME_RATIO)
+    else:
+        BEFORE_TIME = LAG_TIME
+    AFTER_TIME = CLIP_TIME - BEFORE_TIME
+
+
 def get_inst_higher(chat_data):
+    set_time()
+
     temp_time = cmn_func.time_to_seconds(chat_data[list(chat_data.keys())[0]]['elapsedTime'])
     chat_cnt_dict = dict()
     chat_cnt_dict[temp_time] = 1
@@ -48,6 +65,7 @@ def get_inst_higher(chat_data):
 
     return funny_point_list
 
+
 if __name__ == '__main__':
     # video_id = input('plz input video_id:')
     video_id = 'SeWzXV0fYOM'
@@ -56,6 +74,6 @@ if __name__ == '__main__':
 
     funny_point_list = [get_inst_higher(chat_data)]
 
-    number_line = cmn_func.time_to_number_line(funny_point_list)
+    funny_clip = cmn_func.time_to_funny_clip(funny_point_list)
 
-    print(number_line)
+    print(funny_clip)
