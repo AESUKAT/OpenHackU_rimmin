075b99addefa0c16338728a9f409d3d2bf58cf09
AESUKAT <tkmjohu@gmail.com>
2022-03-12 00:53:54 +0900

    URL変更

--- dB_determination.py
+++ dB_determination.py
@@ -23,7 +23,6 @@ def dB_determination(YouTube_URL):
     #del_file(mp4_file_path, wav_file_path)
 
     print('全完')
-    print(chapter_list)
     return chapter_list
 
 # 動画をダウンロードする
@@ -154,5 +153,5 @@ def del_file(mp4_file_path, wav_file_path):
         os.remove(wav_file_path)
     print('証拠は残さない')
 
-#if __name__ == "__main__":
-chapter_list = dB_determination('https://www.youtube.com/watch?v=PJoTdBQodOE')
+if __name__ == "__main__":
+    chapter_list = dB_determination('https://www.youtube.com/watch?v=PJoTdBQodOE')
